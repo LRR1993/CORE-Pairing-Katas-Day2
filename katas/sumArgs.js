caf2defb26802caae1bc31d6989d95a3ec234e68
Northcoders4 <northcoders4@Northcoders4s-MacBook-Pro.local>
2019-02-28 18:17:41 +0000

    sumArgs complete

--- katas/sumArgs.js
+++ katas/sumArgs.js
@@ -1,5 +1,7 @@
 /* The function sumArgs should accept any number of arguments, and add them together. If given one argument, it should return that. If it is not given arguments, it should return 0 */
 
-function sumArgs() {}
+function sumArgs(...args) {
+  return args.reduce((acc, num) => acc + num, 0);
+}
 
 module.exports = sumArgs;
--- spec/sumArgs.spec.js
+++ spec/sumArgs.spec.js
@@ -1,7 +1,18 @@
-// const sumArgs = require('../katas/sumArgs');
+const { expect } = require('chai');
+const sumArgs = require('../katas/sumArgs');
 
 describe('sumArgs', () => {
-  it('describe what happens if this test succeeds here', () => {
-    // expect(sumArgs()).to...
+  it('returns the some of one argrument', () => {
+    expect(sumArgs()).to.equal(0);
+    expect(sumArgs(1)).to.equal(1);
+    expect(sumArgs(2)).to.equal(2);
+  });
+  it('returns the some of two argruments', () => {
+    expect(sumArgs(1, 0)).to.equal(1);
+    expect(sumArgs(1, 2)).to.equal(3);
+  });
+  it('returns the some of three or more argruments', () => {
+    expect(sumArgs(1, 0, 5, 6)).to.equal(12);
+    expect(sumArgs(1, 2, 1, 1)).to.equal(5);
   });
 });
